1c7719aea76232be8e3ecf3e2ac85d9dce1c055f
GONTARDLukas <lukas.gontard@etu.univ-smb.fr>
2025-10-06 10:04:49 +0200

    finding every palindrom in 10**n - 10**(n+1) range

--- #4.py
+++ #4.py
@@ -1,10 +1,26 @@
 def palindromProduct(n):
-    for i in range(10**n, 10**(n+1)-1):
-        for y in range(10**n, 10**(n+1)-1):
-            if i*y
+    palindrom = []
+    for i in range(10**n, 10**(n+1)):
+        for y in range(10**n, 10**(n+1)):
+            if isPalindrom(str(i*y)):
+                palindrom+=[(i,y)]
+    return palindrom
 def isPalindrom(x):
-    if((len(x)-1)%2==1):
-        x = x[:(len(x)-1)//2-1]+x[(len(x)-1)//2:]
-    for i in range((len(x)-1)//2):
-        x
-print(palindromProduct(3))
+    print(x)
+    if(len(x)%2==1):
+        x =x[:(len(x))//2]+x[(len(x))//2+1:]
+    print(x)
+    isPalindrom=True
+    for i in range(len(x)//2):
+        if x[i] != x[len(x)-i-1]:
+            print(x[i], x[len(x)-i-1],x)
+            isPalindrom = False
+            break
+    if isPalindrom:
+        print(x, "True")
+    return isPalindrom
+print(isPalindrom("aabaa"))
+print(palindromProduct(1))
+
+
+print(isPalindrom(str(99*91)))
